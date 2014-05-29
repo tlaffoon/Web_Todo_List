6bdc449a9b40e0ce4f33e25967e9c55aae380582
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-29 10:15:35 -0500

    Basic functionality present - can add items to list.

--- public/todo_list.php
+++ public/todo_list.php
@@ -4,55 +4,79 @@
 </head>
 <body>
 
+	<form method="POST" action="">
+
+		<label for="add_item">Add Item: </label>
+		<input id="add_item" name="add_item" type="text" placeholder="Item Here">
+
+		<button type="submit">SUBMIT</button>
+
+	</form>
+
 	<?php
 
 	/* -------------------------------------- */
 
 	// OPEN FILE TO POPULATE LIST
 
-	function open_file($file) {
+	function open_file($filename) {
 
-	    // echo "Please enter filename to open (default: ./data/list.txt): ";
-	    // $filename = get_input();
-
-	    if (empty($file) ) {
-	        $filename = './data/list.txt';
-	    }
+	    // if (empty($file) ) {
+	    //     $filename = './data/list.txt';
+	    // }
 
 	    $handle = fopen($filename, 'r');
 	    $contents = trim(fread($handle, filesize($filename)));
 	    $list = explode("\n", $contents);
 	    fclose($handle);
+	    
 	    return $list;
 	}
 
 	/* -------------------------------------- */
 
-	$file = '';
-	$list = open_file($file);
+	// SAVE LIST TO FILE
 
-	//$list_items = ['walk dog', 'wash car', 'study code'];
+	function append_to_file($new_item, $filename = './data/list.txt') {
 
-	echo "<ul>";
-
-	foreach ($list as $key => $value) {
-		echo "<li>$value</li>";
+	    if (file_exists($filename)) {
+	        $handle = fopen($filename, 'a');
+	        fwrite($handle, $new_item . PHP_EOL);
+	        fclose($handle);
+	    }  // else nothing.
+	    
+	    return "Successfully saved item: {$new_item} to your list.";
 	}
 
-	echo "</ul>";
+	/* -------------------------------------- */
+	
+	// Outputs List to Browser
 
-	?>
+	function output_list($list) {
 
-<form method="POST" action="./todo_list.php">
+		echo "<ol>";
+		foreach ($list as $key => $item) {
+			echo "<li>$item  <a href=''>Mark Completed</a> </li>";
+		}
+		echo "</ol>";
 
-	<label for="add_item">Add Item: </label>
-	<input id="add_item" type="text" placeholder="Item Here">
+	}
 
-	<button type="submit">SUBMIT</button>
+	/* -------------------------------------- */
 
-</form>
+	// Create new_item variable from form $_POST data
+	$new_item = implode("", $_POST);
 
+	// Append new item to file data
+	echo append_to_file($new_item);
 
+	// Populate list array from data file
+	$list_items = open_file('./data/list.txt');
+
+	// Output List to Browser
+	output_list($list_items);
+
+?>
 
 </body>
 </html>
