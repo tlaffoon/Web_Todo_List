e23ab05e64fbd0a23f81bff9028fc1d94b1f0f8d
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-30 01:20:26 -0500

    Completed exercise - although an excruciating process - we now have a list that functions within the parameters specified.

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,9 +1,87 @@
+<?php
+
+/* -------------------------------------- */
+
+// OPEN FILE TO POPULATE LIST
+
+function open_file($filename = './data/list.txt') {
+
+    $handle = fopen($filename, 'r');
+    $contents = trim(fread($handle, filesize($filename)));
+    $list = explode("\n", $contents);
+    fclose($handle);
+
+    return $list;
+}
+
+/* -------------------------------------- */
+
+// OUTPUTS LIST FROM ARRAY AS HTML
+
+function output_list($list) {
+
+	$string = '';
+
+	foreach ($list as $key => $item) {
+		$removeLink = "<a href=\"todo_list.php?removeIndex={$key} \">Remove</a>";
+		echo "<li>{$item} - {$removeLink}</li>";
+	}
+}
+
+/* -------------------------------------- */
+
+// Overwrites Existing Save File With Current List
+
+function save_to_file($list, $filename = './data/list.txt') {
+
+	$handle = fopen($filename, 'w');
+	$string = implode("\n", $list);
+	fwrite($handle, $string);
+	fclose($handle);
+
+	return "Succesfully saved list to file.";
+}
+
+// add function dedupe list
+// add function sort list alphabetically
+// add function set priority of items ... sort by priority
+
+/* -------------------------------------- */
+
+/*  BEGIN MAIN LOGIC  */
+
+// Populate list array from data file
+$list = open_file();
+
+// Check for post data, continue if not empty
+if (!empty($_POST)) {
+
+	// Assign new_item variable to value posted from form.
+	$new_item = $_POST['add_item'];
+
+	// Add new item to existing array
+	$list[] = $new_item;
+
+	save_to_file($list);
+}
+
+// If get request exists to remove item, do so.
+if (isset($_GET['removeIndex'])) {
+ 	unset($list[$_GET['removeIndex']]);
+ 	array_values($list);
+ 	// save new array to file
+ 	save_to_file($list);
+ }
+
+?>
+
 <html>
 <head>
 	<title>Todo List Web App</title>
 </head>
 <body>
 
+<!-- Add Item Form																-->
 	<form method="POST" action="">
 
 		<label for="add_item">Add Item: </label>
@@ -12,71 +90,26 @@
 		<button type="submit">SUBMIT</button>
 
 	</form>
+<!-- 																			-->
 
-	<?php
 
-	/* -------------------------------------- */
+<!-- Remove Item Form															
+	<form method="GET" action="">
 
-	// OPEN FILE TO POPULATE LIST
+		<label for="remove_item">Remove Item: </label>
+		<input id="remove_item" name="remove_item" type="text" placeholder="Item #">
 
-	function open_file($filename) {
+		<button type="submit">SUBMIT</button>
 
-	    // if (empty($file) ) {
-	    //     $filename = './data/list.txt';
-	    // }
+	</form>
+	 																			-->
 
-	    $handle = fopen($filename, 'r');
-	    $contents = trim(fread($handle, filesize($filename)));
-	    $list = explode("\n", $contents);
-	    fclose($handle);
-	    
-	    return $list;
-	}
-
-	/* -------------------------------------- */
-
-	// SAVE LIST TO FILE
-
-	function append_to_file($new_item, $filename = './data/list.txt') {
-
-	    if (file_exists($filename)) {
-	        $handle = fopen($filename, 'a');
-	        fwrite($handle, $new_item . PHP_EOL);
-	        fclose($handle);
-	    }  // else nothing.
-	    
-	    return "Successfully saved item: {$new_item} to your list.";
-	}
-
-	/* -------------------------------------- */
-	
-	// Outputs List to Browser
-
-	function output_list($list) {
-
-		echo "<ol>";
-		foreach ($list as $key => $item) {
-			echo "<li>$item  <a href=''>Mark Completed</a> </li>";
-		}
-		echo "</ol>";
-
-	}
-
-	/* -------------------------------------- */
-
-	// Create new_item variable from form $_POST data
-	$new_item = implode("", $_POST);
-
-	// Append new item to file data
-	echo append_to_file($new_item);
-
-	// Populate list array from data file
-	$list_items = open_file('./data/list.txt');
-
-	// Output List to Browser
-	output_list($list_items);
-
-?>
+	<ol>
+		<?php
+			// Output List to Browser
+			echo output_list($list);
+		?>
+	</ol>
 
 </body>
 </html>
